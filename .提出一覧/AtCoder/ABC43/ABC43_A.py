af7019df46e8e836b1272b82d97e180c6c03d5a1
Yukikazari <haya_nanakusa793@yahoo.co.jp>
2020-07-25 05:05:21 +0900

    change

---
+++ .提出一覧/AtCoder/ABC43/ABC43_A.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+r = 0
+
+for i in range(1, N + 1):
+    r += i
+
+print(r)
---
+++ Template/input.py
@@ -0,0 +1,16 @@
+# int input
+
+int(input())
+
+map(int, input().split())
+
+list(map(int, input().split()))
+
+
+# str input
+
+input()
+
+map(str, input().split())
+
+list(map(str, input().split()))
